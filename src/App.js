05efa8b09f940f818c410cee596f25277de4f455
Hubert Zając <hubert.zajac92@gmail.com>
2022-05-15 07:33:37 +0200

    add asynchronous data fetch

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './App.css';
 
 export const App = () => {
+
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const res = await fetch("https://jsonplaceholder.typicode.com/users");
+      const data = await res.json();
+      setUsers(data);
+    })()
+  }, [])
+
+  console.log(users);
+
   return (
     <div className="App">
       123
